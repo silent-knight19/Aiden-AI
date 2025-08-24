f72be69f6ab30c6a61933715742651e90e7b43fe
silent-knight19 <sachinsinghtomar7749@gmail.com>
2025-08-24 23:15:22 +0530

    updated the routes

--- Frontend/Routes/AppRoutes.jsx
+++ Frontend/Routes/AppRoutes.jsx
@@ -1,13 +1,15 @@
 import react from "react";
 import { Route,BrowserRouter,Routes } from "react-router-dom";
 import Login from "../Pages/Login";
+import Home from "../Pages/Home";
+import Signup from "../Pages/Signup";
 const appRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<div>Home</div>} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<div>Register</div>} />
+                <Route path="/" element={<Home/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/register" element={<Signup/>} />
             </Routes>
         </BrowserRouter>
     );
